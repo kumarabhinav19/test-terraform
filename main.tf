3843dcac59c8d1e389796ae3dd041b3a7f37c08e
kumarabhinav19 <abhinavtiwary287@gmail.com>
2021-05-14 20:52:14 +0530

    abhogaya

--- main.tf
+++ main.tf
@@ -1,46 +1,132 @@
-locals {
-  enabled                                         = module.this.enabled
-  enable_default_security_group_with_custom_rules = var.enable_default_security_group_with_custom_rules && local.enabled ? 1 : 0
-  enable_internet_gateway                         = var.enable_internet_gateway && local.enabled ? 1 : 0
-  additional_cidr_blocks_defined                  = local.enabled && var.additional_cidr_blocks != null ? true : false
-  additional_cidr_blocks                          = local.additional_cidr_blocks_defined ? var.additional_cidr_blocks : []
+
+
+//create vpc 
+resource "aws_vpc" "my_first_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "production-vpc"
+  }
 }
 
-module "label" {
-  source  = "cloudposse/label/null"
-  version = "0.24.1"
 
-  context = module.this.context
+
+//create internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_first_vpc.id
+
+  tags = {
+    Name = "main"
+  }
 }
 
-resource "aws_vpc" "default" {
-  count                            = local.enabled ? 1 : 0
-  cidr_block                       = var.cidr_block
-  instance_tenancy                 = var.instance_tenancy
-  enable_dns_hostnames             = var.enable_dns_hostnames
-  enable_dns_support               = var.enable_dns_support
-  enable_classiclink               = var.enable_classiclink
-  enable_classiclink_dns_support   = var.enable_classiclink_dns_support
-  assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
-  tags                             = module.label.tags
+//create aws route table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_first_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id  = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "route-table"
+  }
+}
+// create subnet
+resource "aws_subnet" "my_first_subnet" {
+  vpc_id            = aws_vpc.my_first_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "production-subnet"
+  }
 }
 
-# If `aws_default_security_group` is not defined, it would be created implicitly with access `0.0.0.0/0`
-resource "aws_default_security_group" "default" {
-  count  = local.enable_default_security_group_with_custom_rules
-  vpc_id = join("", aws_vpc.default.*.id)
-
-  tags = merge(module.label.tags, { Name = "Default Security Group" })
+//create route table associate with subnet
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.my_first_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
 }
 
-resource "aws_internet_gateway" "default" {
-  count  = local.enable_internet_gateway
-  vpc_id = join("", aws_vpc.default.*.id)
-  tags   = module.label.tags
+//create security group
+resource "aws_security_group" "allow_web" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.my_first_vpc.id
+
+  ingress {
+    description = "https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http_https"
+  }
 }
 
-resource "aws_vpc_ipv4_cidr_block_association" "default" {
-  for_each   = toset(local.additional_cidr_blocks)
-  vpc_id     = join("", aws_vpc.default.*.id)
-  cidr_block = each.key
+//create network interface
+resource "aws_network_interface" "test" {
+  subnet_id       = aws_subnet.my_first_subnet.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+//create elastic ip for nic
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.test.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+//create aws server
+
+resource "aws_instance" "web-server" {
+  ami               = "ami-0742b4e673072066f"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.test.id
+  }
+
+  tags = {
+    Name = "web-server"
+  }
 }
